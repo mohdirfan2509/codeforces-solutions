eb1eff115d847d0bec601f0ebb922edffbd8e54c
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-04-08 22:50:12 +0530

    Square of rectanghttps://codeforces.com/group/zOa9U7TcXb/contest/683930/problem/Parvez

--- contest-jack/Complexity-06-04-2026/Square-of-Rectangles.cpp
+++ contest-jack/Complexity-06-04-2026/Square-of-Rectangles.cpp
@@ -1,3 +1,4 @@
+// Problem Link : https://codeforces.com/group/zOa9U7TcXb/contest/683930/problem/Parvez
 
 #include<iostream>
 #include<bits/stdc++.h>
@@ -9,8 +10,16 @@ int main(){
     for(int i=0;i<test_cases;i++){
         int l1,b1,l2,b2,l3,b3;
         cin >> l1 >> b1 >> l2 >> b2 >> l3 >> b3;
-        if(l1 ==l2 && l2==l3 && b1+b2+b3=l1){
-
+        if(l1 ==l2 && l2==l3 && b1+b2+b3==l1){
+            cout << "YES" << endl;
+        }else if(b1==b2 && b2==b3 && l1+l2+l3==b1){
+            cout << "YES" << endl;
+        }else if(b1==b2+b3 && l2==l3 && l1+l2==b1){
+            cout << "YES" << endl;
+        }else if(l1==l2+l3 && b2==b3 && b1+b3==l1){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
         }
     }
 }
