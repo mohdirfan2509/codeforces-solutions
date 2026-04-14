880d6fa2cd2d595b82ea779229615769d7959a14
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-04-14 22:52:03 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A

---
+++ simple-problems/Hello.cpp
@@ -0,0 +1,10 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   string s;
+   cin >> s;
+   cout << "Hello, " << s;
+}
