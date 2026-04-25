9bdb0e00835a40d198dcba23802025eae53896c9
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-04-25 09:42:50 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C

---
+++ rating-900-problems/Simple-Calculator.cpp
@@ -0,0 +1,13 @@
+// Question Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+ 
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    long long x,y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << x+y << endl;;
+    cout << x << " * " << y << " = " << x*y << endl;;
+    cout << x << " - " << y << " = " << x-y << endl;;
+    
+}
