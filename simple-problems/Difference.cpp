661e41bd7c2061a91f9196d8bae9ec841fd6cac1
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-03 12:35:57 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D

---
+++ simple-problems/Difference.cpp
@@ -0,0 +1,11 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long a,b,c,d;
+    cin >> a >> b >> c >> d ;
+    cout << "Difference = " << (a*b)-(c*d) << endl;
+  
+}
+
