907be8ed86ebcdd8f19d2e5c8386aedb28aa3a78
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-05 18:31:36 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E

---
+++ simple-problems/Area-of-a-Circle.cpp
@@ -0,0 +1,13 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double r;
+    cin >> r;
+    double pi=3.141592653;
+    cout << fixed << setprecision(9) << pi*r*r << endl;
+  
+}
+
