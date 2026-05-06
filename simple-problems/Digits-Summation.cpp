87cadbeb27b7939ae59d38686f233c9fd724c03b
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-06 09:48:24 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F

---
+++ simple-problems/Digits-Summation.cpp
@@ -0,0 +1,12 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   long long n,m;
+   cin >> n >> m;
+   cout << n%10 + m%10 << endl;
+  
+}
+
