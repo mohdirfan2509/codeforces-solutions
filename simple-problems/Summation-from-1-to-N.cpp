2edfd5d6260640ef7fbc2a5be5b1b6af3727038e
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-07 09:30:12 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G

---
+++ simple-problems/Summation-from-1-to-N.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    long long sum = n*(n+1)/2;
+    cout << sum << endl;
+   
+  
+}
+
