aebb7eafbe9205573a509dbb6f1f0e569d714c16
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-12 17:20:55 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L

---
+++ simple-problems/The-Brothers.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string f1,s1,f2,s2;
+    cin >> f1 >> s1 >> f2 >> s2 ;
+    string ans=(s1==s2) ? "SSSSARE Brothers" : "NOT" ;
+    cout << ans<< endl;
+   
+  
+}
+
