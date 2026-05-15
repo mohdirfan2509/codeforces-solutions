6e55e2a47f9749751ff7b478b9cd8294049b485d
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-15 12:22:03 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/O

---
+++ simple-problems/Calculator.cpp
@@ -0,0 +1,20 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/O
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    char s;
+    cin >> a >> s >> b;
+    if(s=='+'){
+        cout << a+b << endl;
+    }else if(s=='-'){
+        cout << a-b << endl;
+    }else if(s=='*'){
+        cout << a*b << endl;
+    }else{
+        cout << a/b << endl;
+    }
+  
+}
+
