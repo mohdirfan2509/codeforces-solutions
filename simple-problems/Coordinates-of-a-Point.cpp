a4865d48cf6fab6d790ed54007a1ce4e8758fee2
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-17 15:51:40 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Q

---
+++ simple-problems/Coordinates-of-a-Point.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Q
+#include<iostream>
+using namespace std;
+
+int main(){
+   float x,y;
+   cin >> x >> y ;
+   if(x==0 && y==0){
+    cout << "Origem" << endl;
+   }else if(x==0 && y!=0){
+    cout << "Eixo Y" << endl;
+   }else if(x!=0 && y==0){
+    cout << "Eixo X" << endl;
+   }else if(x>0 && y>0){
+    cout << "Q1" << endl;
+   }else if(x<0 && y>0){
+    cout << "Q2" << endl;
+   }else if(x<0 && y<0){
+    cout << "Q3" << endl;
+   }else{
+    cout << "Q4" << endl;
+   }
+}
+
