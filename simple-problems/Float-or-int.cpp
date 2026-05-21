fb16563626eb43333541e213f26473d51b90f3f6
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-21 21:00:23 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/U

---
+++ simple-problems/Float-or-int.cpp
@@ -0,0 +1,17 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/U
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float n;
+    cin >> n;
+
+    int a=n;
+    if(n-a==0){
+        cout << "int" << " " << a;
+    }else{
+        cout << "float" << " " << a << " " << n-a ;
+    }
+}
+
