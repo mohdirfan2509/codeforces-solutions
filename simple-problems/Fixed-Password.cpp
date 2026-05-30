672647b048485384a6d3e7ed4d4d53313f54820f
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-05-30 12:38:47 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D

---
+++ simple-problems/Fixed-Password.cpp
@@ -0,0 +1,18 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while(true){
+        int n;
+        cin >> n;
+        if(n==1999){
+            cout << "Correct" << endl;
+            break;
+        }else{
+            cout << "Wrong" << endl;
+        }
+    }
+
+}
