1389fd0bca1eaec161ff9996606dc5507a67c4fe
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-06-01 09:44:25 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F

---
+++ simple-problems/Multiplication-table.cpp
@@ -0,0 +1,13 @@
+// Question Link:https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  for(int i=1;i<=12;i++){
+    cout << n << " "  << "*" << " " << i << " " << "=" << " " << n*i  << endl;
+  } 
+}
+
