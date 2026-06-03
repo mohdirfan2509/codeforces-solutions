a7df982fbe4e88ab39c18384283bc9df00e011a0
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-06-03 17:16:21 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/H

---
+++ simple-problems/One-Prime.cpp
@@ -0,0 +1,26 @@
+// Question Link:https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/H
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x){
+    if(x<2) return false;
+    for(int i=2;i*i<=x;i++){
+        if(x%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+  int n;
+  cin >>  n;
+  if(isPrime(n)){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+
+}
+
