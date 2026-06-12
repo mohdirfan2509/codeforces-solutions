8229de2b6c0adb85fe3e33602b37d09327d2073e
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-06-12 12:09:33 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/O

---
+++ simple-problems/Pyramid.cpp
@@ -0,0 +1,16 @@
+// Question Link:https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/O
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout << "*" ;
+    }
+    cout << endl;
+  }
+}
+
