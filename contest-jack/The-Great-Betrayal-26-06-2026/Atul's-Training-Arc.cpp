377a64778cfc268039c5b48bd7bc47271f6d9652
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-06-27 11:31:17 +0530

    https://codeforces.com/group/zOa9U7TcXb/contest/700735/problem/G

---
+++ contest-jack/The-Great-Betrayal-26-06-2026/Atul's-Training-Arc.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://codeforces.com/group/zOa9U7TcXb/contest/700735/problem/G
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    int ans = 0;
+    int dayNum=1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= dayNum) {
+            ans++;
+            dayNum++;
+        }
+    }
+    cout << ans << endl;
+}
