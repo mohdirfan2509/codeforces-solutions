11e5f258622ac1c32fe58a5072482eda26a9b00d
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-07-16 14:33:30 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/N

---
+++ ICPC-Assiut-University-Community/Sheet #3 (Arrays)/Check-Code.cpp
@@ -0,0 +1,28 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/N
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    string s;
+    cin >> a >> b >> s;
+    bool isInPosition=false;
+    bool areDigits=true;
+    if(s[a+1+1]=='-'){
+        isInPosition=true;
+    }
+    for(int i=0;i<s.size();i++){
+        if(i != a+1+1){
+            if(s[i]-'0' < 0 || s[i]-'0' > 9){
+                areDigits=false;
+            }
+        }
+    }
+    if(isInPosition && areDigits){
+        cout << "Yes" << endl; 
+    }else{
+        cout << "No" << endl; 
+    }
+}
