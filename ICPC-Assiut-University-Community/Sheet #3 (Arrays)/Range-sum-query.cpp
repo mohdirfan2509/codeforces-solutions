e9f9a547ae1661d43b025b269378db2cd6f41aea
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-07-31 18:10:52 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y

--- ICPC-Assiut-University-Community/Sheet #3 (Arrays)/Range-sum-query.cpp
+++ ICPC-Assiut-University-Community/Sheet #3 (Arrays)/Range-sum-query.cpp
@@ -6,22 +6,30 @@
 using namespace std;
 
 int main() {
-    long long  n, q;
+    long long n, q;
     cin >> n >> q;
-    vector<long long > arr(n);
-    for (long long  i = 0; i < n; i++) {
+    vector<long long> arr(n);
+    for (long long i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    vector<pair<long long ,long long >> queries(q);
-    for(long long  i=0;i<q;i++){
+    vector<pair<long long, long long>> queries(q);
+    for (long long i = 0; i < q; i++) {
         cin >> queries[i].first;
         cin >> queries[i].second;
     }
-    for(long long  i=0;i<q;i++){
-        long long  sum=0;
-        for(long long  j=queries[i].first-1;j<=queries[i].second-1;j++){
-            sum=sum+arr[j];
+    vector<long long> pf(n, 0);
+    long long psum = 0;
+    for (long long i = 0; i < n; i++) {
+        psum += arr[i];
+        pf[i] = psum;
+    }
+    for (long long i = 0; i < q; i++) {
+        long long start = queries[i].first - 1;
+        long long end = queries[i].second - 1;
+        if (start == 0) {
+            cout << pf[end] << endl;
+        } else {
+            cout << pf[end] - pf[start-1] << endl;
         }
-        cout << sum << endl;
     }
 }
