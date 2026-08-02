21ecc968ef84c6309c3779b3c78e78c12b61c0b9
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-02 12:49:54 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219774

---
+++ ICPC-Assiut-University-Community/Sheet #4 (Strings)/Create-A-New-String.cpp
@@ -0,0 +1,13 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s,t;
+    cin >> s >> t;
+    cout << s.size() << " " << t.size() << endl;
+    cout << s << " " << t << endl;
+}
