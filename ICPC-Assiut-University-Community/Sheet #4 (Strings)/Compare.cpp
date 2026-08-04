5db3a55494214c7316f8ad225483744baa81b698
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-04 12:19:49 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/C

---
+++ ICPC-Assiut-University-Community/Sheet #4 (Strings)/Compare.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/C
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string x,y;
+    cin >> x >> y;
+    string ans= x<=y ? x : y;
+    cout << ans << endl;
+  
+}
