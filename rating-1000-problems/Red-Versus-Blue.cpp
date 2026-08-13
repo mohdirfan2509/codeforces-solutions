a5f6497f144b9a07d1ad6ea05f1066cdfaa02716
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-13 22:10:41 +0530

    https://codeforces.com/problemset/problem/1659/A

--- rating-1000-problems/Red-Versus-Blue.cpp
+++ rating-1000-problems/Red-Versus-Blue.cpp
@@ -1,13 +1,33 @@
-// Question Link: https://codeforces.com/problemset/problem/1883/C
-#include<iostream>
-#include<bits/stdc++.h>
+// Question Link: https://codeforces.com/problemset/problem/1659/A
+#include <bits/stdc++.h>
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int test_cases;
     cin >> test_cases;
-    for(int i=0;i<test_cases;i++){
-       
-    }
+    for (int i = 0; i < test_cases; i++) {
+        int n, r, b;
+        cin >> n >> r >> b;
 
+        string s;
+
+        int lengthOfRed=r/(b+1);
+
+        int extraRed=r%(b+1);
+
+        for(int times=1;times<=b+1;times++){
+
+            for(int j=0;j<lengthOfRed;j++)  s+='R';
+
+            if(extraRed>0){
+                s+='R';
+                extraRed--;
+            }
+
+            if(times != b+1) s+='B';
+        }
+        cout << s << endl;
+    }
 }
