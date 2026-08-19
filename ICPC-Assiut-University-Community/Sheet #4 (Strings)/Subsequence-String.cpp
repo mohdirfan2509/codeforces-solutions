007d8145bf60e1316a9782d5e87bc0f78aa42dd7
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-19 22:49:43 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/M

--- ICPC-Assiut-University-Community/Sheet #4 (Strings)/Subsequence-String.cpp
+++ ICPC-Assiut-University-Community/Sheet #4 (Strings)/Subsequence-String.cpp
@@ -1,1 +1,24 @@
-helllo
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/D
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string val = "hello";
+    int p = 0;
+    for (char x : s) {
+        if (p <= 4 && x == val[p]) {
+            p++;
+        }
+    }
+    if (p == 5) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
