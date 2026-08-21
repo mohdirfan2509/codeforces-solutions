15c191cd57e1e3a0c07cd5ec804baa5cd44f34eb
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-21 19:50:31 +0530

    https://codeforces.com/problemset/problem/1632/B

--- rating-1000-problems/Roof-Construction.cpp
+++ rating-1000-problems/Roof-Construction.cpp
@@ -8,6 +8,25 @@ int main() {
     int test_cases;
     cin >> test_cases;
     for (int i = 0; i < test_cases; i++) {
-       
+        long long n;
+        cin >> n;
+        n--;
+        long long msb = log2(n);
+        vector<long long> ans;
+        long long num = pow(2, msb) - 1;
+        while (num >= 0) {
+            ans.push_back(num);
+            num--;
+        }
+        num = pow(2, msb);
+        while (num <= n) {
+            ans.push_back(num);
+            num++;
+        }
+
+        for(auto it:ans){
+            cout << it << " ";
+        }
+        cout << endl;
     }
 }
