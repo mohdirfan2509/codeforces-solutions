d363d109c0273bb498d218df84795f940e616b27
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-25 20:17:51 +0530

    https://codeforces.com/problemset/problem/489/B

--- CP-classes/homework/BerSU-Ball.cpp
+++ CP-classes/homework/BerSU-Ball.cpp
@@ -1,4 +1,4 @@
-// Question Link: https://codeforces.com/problemset/problem/160/A
+// Question Link: https://codeforces.com/problemset/problem/489/B
 #include <bits/stdc++.h>
 
 #include <iostream>
@@ -6,5 +6,45 @@
 using namespace std;
 
 int main() {
-   
+    int n;
+    cin >> n;
+    vector<int> boys(n);
+    for (int i = 0; i < n; i++) {
+        cin >> boys[i];
+    }
+
+    int m;
+    cin >> m;
+    vector<int> girls(m);
+    for (int i = 0; i < m; i++) {
+        cin >> girls[i];
+    }
+    sort(boys.begin(), boys.end());
+    unordered_map<int, int> uGirls;
+    for (int i : girls) {
+        uGirls[i]++;
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (uGirls.find(boys[i] - 1) != uGirls.end()) {
+            uGirls[boys[i] - 1]--;
+            ans++;
+            if (uGirls[boys[i] - 1] <= 0) {
+                uGirls.erase(boys[i] - 1);
+            }
+        } else if (uGirls.find(boys[i]) != uGirls.end()) {
+            uGirls[boys[i]]--;
+            ans++;
+            if (uGirls[boys[i]] <= 0) {
+                uGirls.erase(boys[i]);
+            }
+        } else if (uGirls.find(boys[i] + 1) != uGirls.end()) {
+            uGirls[boys[i] + 1]--;
+            ans++;
+            if (uGirls[boys[i] + 1] <= 0) {
+                uGirls.erase(boys[i] + 1);
+            }
+        }
+    }
+    cout << ans << endl;
 }
