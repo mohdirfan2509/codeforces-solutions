8bced320cc3def32770acf52f69214054f8b9e33
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-08-31 18:41:52 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/T

--- ICPC-Assiut-University-Community/Sheet #4 (Strings)/URL.cpp
+++ ICPC-Assiut-University-Community/Sheet #4 (Strings)/URL.cpp
@@ -1,4 +1,4 @@
-// Question Link:
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/T
 #include <bits/stdc++.h>
 
 #include <iostream>
@@ -6,5 +6,32 @@
 using namespace std;
 
 int main() {
-   
+    string s;
+    cin >> s;
+
+    vector<string> words;
+
+    stringstream ss(s);
+    string token;
+    while (getline(ss, token, '?')) {
+        words.push_back(token);
+    }
+
+    string part = words[1];
+    words.clear();
+    token.clear();
+    stringstream ss2(part);
+    while (getline(ss2, token, '&')) {
+        words.push_back(token);
+    }
+
+    for(string a:words){
+
+        for(char x:a){
+            if(x== '='){
+                x=': ';
+            }
+        }
+        cout << a << endl;
+    }
 }
