346224fea826af9e31ff13941827c4f1b761282b
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-09-07 17:57:55 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A

---
+++ ICPC-Assiut-University-Community/Sheet #5 (Functions)/Add.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A
+#include <iostream>
+using namespace std;
+
+int summation(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << summation(a, b) << endl;
+}
