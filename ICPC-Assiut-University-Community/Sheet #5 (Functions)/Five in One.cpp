9bc1b5580fc9a9d235a81bfcf2a6f0d418da0ff0
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-09-21 19:36:03 +0530

    https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/O

---
+++ ICPC-Assiut-University-Community/Sheet #5 (Functions)/Five in One.cpp
@@ -0,0 +1,101 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/O
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+int maximum(vector<int>& arr) {
+    int maxElement = INT_MIN;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        maxElement = max(maxElement, arr[i]);
+    }
+}
+
+int minimum(vector<int>& arr) {
+    int minElement = INT_MAX;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        minElement = min(minElement, arr[i]);
+    }
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int noOfPrimes(vector<int>& arr) {
+    int n = arr.size();
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+bool isPalindrome(string s) {
+    string x = s;
+    reverse(s.begin(), s.end());
+    return s == x;
+}
+
+int noOfPalindromes(vector<int>& arr) {
+    int n = arr.size();
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (isPalindrome(to_string(arr[i]))) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int noOfDivisors(int n) {
+    int count = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int elementWithMaxNoOfDivisors(vector<int>& arr) {
+    int n = arr.size();
+    pair<int, int> currMaxAndElement = {0, 0};
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (noOfDivisors(arr[i]) > currMaxAndElement.first) {
+            currMaxAndElement = {noOfDivisors(arr[i]), arr[i]};
+            ans = arr[i];
+        } else if (noOfDivisors(arr[i]) == currMaxAndElement.first) {
+            ans = max(arr[i], currMaxAndElement.second);
+            currMaxAndElement = {noOfDivisors(arr[i]), ans};
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "The maximum number : " << maximum(arr) << endl;
+    cout << "The minimum number : " << minimum(arr) << endl;
+    cout << "The number of prime numbers : " << noOfPrimes(arr) << endl;
+    cout << "The number of palindrome numbers : " << noOfPalindromes(arr) << endl;
+    cout << "The number that has the maximum number of divisors : " << elementWithMaxNoOfDivisors(arr) << endl;
+}
